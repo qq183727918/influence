ffc6681985a41d4696581aca9b9f1de19ef7ff96
lxqiang95 <lxq13925731395@163.com>
2020-12-04 17:35:39 +0800

    Default Changelist

--- debug/verification_test.py
+++ debug/verification_test.py
@@ -4,30 +4,26 @@
 # @Site     : http:www.cdtest.cn/
 # @File     : verification_test.py
 # @Software : PyCharm
-# from PIL import Image
-#
-# #使用路径导入图片
-# imgName = '1.png'
-# im = Image.open(imgName)
-# #使用 byte 流导入图片
-# # im = Image.open(io.BytesIO(b))
-# # 转化到灰度图
-# imgry = im.convert('L')
-# # 保存图像
-# imgry.save('gray-' + imgName)
+from PIL import Image
+# 转换灰度
+#使用路径导入图片
+imgName = '1.png'
+im = Image.open(imgName)
+#使用 byte 流导入图片
+# im = Image.open(io.BytesIO(b))
+# 转化到灰度图
+imgry = im.convert('L')
+# 保存图像
+imgry.save('gray-' + imgName)
 
-# 二值化，采用阈值分割法，threshold为分割点
-# threshold = 140
-# table = []
-# for j in range(256):
-#     if j < threshold:
-#         table.append(0)
-#     else:
-#         table.append(1)
-# out = imgry.point(table, '1')
-# out.save('b' + imgName)
+#  二值化降噪的过程
+from PIL import Image, ImageEnhance, ImageFilter
 
-import pytesseract
-#  识别
-text = pytesseract.image_to_string('1.png')
-print(f"识别结果：{text}")
+im = Image.open('gray-1.png')
+im = im.filter(ImageFilter.MedianFilter())
+enhancer = ImageEnhance.Contrast(im)
+im = enhancer.enhance(2)
+im = im.convert('1')
+im.show()
+im.save('./1213.png')
+
