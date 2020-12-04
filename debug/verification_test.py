2a97dbb5eef05a947ffef9043804c0f0e71b3339
lxqiang95 <lxq13925731395@163.com>
2020-12-04 18:55:18 +0800

    Default Changelist

--- debug/verification_test.py
+++ debug/verification_test.py
@@ -4,26 +4,36 @@
 # @Site     : http:www.cdtest.cn/
 # @File     : verification_test.py
 # @Software : PyCharm
+def verification():
+    from PIL import Image
+    # 转换灰度
+    # 使用路径导入图片
+    imgName = '1.png'
+    im = Image.open(imgName)
+    # 使用 byte 流导入图片
+    # im = Image.open(io.BytesIO(b))
+    # 转化到灰度图
+    imgry = im.convert('L')
+    # 保存图像
+    imgry.save('gray-' + imgName)
+
+    #  二值化降噪的过程
+    from PIL import Image, ImageEnhance, ImageFilter
+
+    im = Image.open('gray-1.png')
+    im = im.filter(ImageFilter.MedianFilter())
+    enhancer = ImageEnhance.Contrast(im)
+    im = enhancer.enhance(2)
+    im = im.convert('1')
+    im.show()
+    im.save('./1213.png')
+
+
+# verification()
 from PIL import Image
-# 转换灰度
-#使用路径导入图片
-imgName = '1.png'
-im = Image.open(imgName)
-#使用 byte 流导入图片
-# im = Image.open(io.BytesIO(b))
-# 转化到灰度图
-imgry = im.convert('L')
-# 保存图像
-imgry.save('gray-' + imgName)
+import pytesseract
 
-#  二值化降噪的过程
-from PIL import Image, ImageEnhance, ImageFilter
-
-im = Image.open('gray-1.png')
-im = im.filter(ImageFilter.MedianFilter())
-enhancer = ImageEnhance.Contrast(im)
-im = enhancer.enhance(2)
-im = im.convert('1')
-im.show()
-im.save('./1213.png')
-
+pytesseract.pytesseract.tesseract_cmd = r'D:\Tools\tesseract\Tesseract-OCR/tesseract.exe'
+image = Image.open("gray-1.png")
+code = pytesseract.image_to_string(image, lang="eng", config="-psm 7")
+print(code)
